9c32aaf66c91cca09e1b2ed07b361a70abac2577
Valeria Gabrielli <gabvale53@gmail.com>
2025-12-10 16:51:36 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+
+Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato) */
+
+
+//
