193717815c402a67a413e6a7a7c16c60109516bd
Valeria Gabrielli <gabvale53@gmail.com>
2025-12-10 17:24:10 +0100

    feat: aggiunta funzione accendi alla lampadina

--- js/script.js
+++ js/script.js
@@ -5,5 +5,13 @@ Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare 
 
 //inserisco img e button in js 
 
-const lampadina = document.getElementById('lampoff');
-const bottone = document.getElementById('buttOn');
+const lamp = document.getElementById('lampoff');
+const button = document.getElementById('buttOn');
+
+//devo dire al mio js che quando clicco buttOn la lampadina si deve accendere e cambia anche scritta, lo faccio aggiungendo addEventListener e una definizione di funzione
+
+button.addEventListener('click', function(){
+    lamp.src = "./img/yellow_lamp.png";
+    lamp.alt = "lampadina_accesa"     //Ho anche cambiato il nome dell'alt così da far risultare tutto il tag più chiaro
+    button.textContent = "Spegni"
+});
